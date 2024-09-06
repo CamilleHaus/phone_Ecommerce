7ef9d0fa877084fa5f69b02f76740ca850cab4e8
Camille Haus <camille_haus@hotmail.com>
2024-09-06 12:56:15 -0300

    updated prisma schema

---
+++ prisma/migrations/20240906155602_updated_db_schema/migration.sql
@@ -0,0 +1,50 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `Order` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Order` table. All the data in the column will be lost.
+  - You are about to alter the column `total` on the `Order` table. The data in that column could be lost. The data in that column will be cast from `DoublePrecision` to `Decimal(65,30)`.
+  - You are about to alter the column `price` on the `Product` table. The data in that column could be lost. The data in that column will be cast from `DoublePrecision` to `Decimal(65,30)`.
+  - You are about to drop the `Cart` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `CartItem` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Cart" DROP CONSTRAINT "Cart_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "CartItem" DROP CONSTRAINT "CartItem_cartId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "CartItem" DROP CONSTRAINT "CartItem_productId_fkey";
+
+-- AlterTable
+ALTER TABLE "Order" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "orderItemId" UUID,
+ALTER COLUMN "total" SET DATA TYPE DECIMAL(65,30);
+
+-- AlterTable
+ALTER TABLE "Product" ALTER COLUMN "price" SET DATA TYPE DECIMAL(65,30);
+
+-- DropTable
+DROP TABLE "Cart";
+
+-- DropTable
+DROP TABLE "CartItem";
+
+-- CreateTable
+CREATE TABLE "OrderItem" (
+    "id" UUID NOT NULL,
+    "productId" UUID NOT NULL,
+    "orderId" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+
+    CONSTRAINT "OrderItem_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_orderItemId_fkey" FOREIGN KEY ("orderItemId") REFERENCES "OrderItem"("id") ON DELETE SET NULL ON UPDATE CASCADE;
